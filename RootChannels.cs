5721d226ba0383f55f3e3aa295a0a33ee0b76af3
Sanziana <Sanziana@SANZI>
2017-05-09 21:36:28 +0300

    Class for Slack Channels

---
+++ RootChannels.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+
+namespace RemoteMessages
+{
+    public class Topic
+    {
+        public string value { get; set; }
+        public string creator { get; set; }
+        public int last_set { get; set; }
+    }
+
+    public class Purpose
+    {
+        public string value { get; set; }
+        public string creator { get; set; }
+        public int last_set { get; set; }
+    }
+
+    public class Channel
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public bool is_channel { get; set; }
+        public int created { get; set; }
+        public string creator { get; set; }
+        public bool is_archived { get; set; }
+        public bool is_general { get; set; }
+        public string name_normalized { get; set; }
+        public bool is_member { get; set; }
+        public List<object> members { get; set; }
+        public Topic topic { get; set; }
+        public Purpose purpose { get; set; }
+        public List<object> previous_names { get; set; }
+        public int num_members { get; set; }
+    }
+
+    public class RootChannels
+    {
+        public bool ok { get; set; }
+        public List<Channel> channels { get; set; }
+    }
+}
