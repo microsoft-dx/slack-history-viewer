3e071b6fe05e87708ddf4409ce7eccf274d5972b
Sanziana <Sanziana@SANZI>
2017-05-09 21:45:46 +0300

    It takes the path to a directory as argument and inserts the content of the files within that directory into a database.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using RestoreFromLocal.Models;
+using System;
+using Newtonsoft.Json;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RestoreFromLocal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string directoryPath;
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: ./program.exe directory_path");
+                return;
+            } else {
+                directoryPath = args[0];
+                if (!Directory.Exists(directoryPath))
+                {
+                    Console.WriteLine("Error: The directory path does not exist");
+                    return;
+                }
+            }
+
+            Data database = new Data();
+            database.truncateAllTables();
+            database.insertUsers(directoryPath);
+            database.insertChannels(directoryPath);
+            database.insertMessages(directoryPath);
+        }
+    }
+}
