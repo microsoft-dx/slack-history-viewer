f8de57d3ceacc31c8df919506bbed99e16786203
Sanziana <Sanziana@SANZI>
2017-05-17 00:17:25 +0300

    Added JsonProperty attribute

---
+++ SlackHistoryViewer.Slack/Models/Channel.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SlackHistoryViewer.Slack.Models
+{
+    public class Channel
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("is_channel")]
+        public bool? IsChannel { get; set; }
+
+        [JsonProperty("created")]
+        public int? Created { get; set; }
+
+        [JsonProperty("creator")]
+        public string Creator { get; set; }
+
+        [JsonProperty("is_archived")]
+        public bool? IsArchived { get; set; }
+
+        [JsonProperty("is_general")]
+        public bool? IsGeneral { get; set; }
+
+        [JsonProperty("name_normalized")]
+        public string NameNormalized { get; set; }
+
+        [JsonProperty("is_member")]
+        public bool? IsMember { get; set; }
+
+        [JsonProperty("members")]
+        public IEnumerable<object> Members { get; set; }
+
+        [JsonProperty("topic")]
+        public Topic Topic { get; set; }
+
+        [JsonProperty("purpose")]
+        public Purpose Purpose { get; set; }
+
+        [JsonProperty("previous_names")]
+        public IEnumerable<string> PreviousNames { get; set; }
+
+        [JsonProperty("num_members")]
+        public int? NumMembers { get; set; }
+    }
+}
---
+++ SlackHistoryViewer.Slack/Models/Edited.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SlackHistoryViewer.Slack.Models
+{
+    public class Edited
+    {
+        [JsonProperty("user")]
+        public string User { get; set; }
+
+        [JsonProperty("ts")]
+        public string TimeStamp { get; set; }
+    }
+}
