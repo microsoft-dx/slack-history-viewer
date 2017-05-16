9317303c3637c9d806356a46ecf70c88efcd3389
Sanziana <Sanziana@SANZI>
2017-05-17 00:18:04 +0300

    Added JsonProperty attribute

---
+++ SlackHistoryViewer.Slack/Models/Member.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SlackHistoryViewer.Slack.Models
+{
+    public class Member
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("team_id")]
+        public string TeamId { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("deleted")]
+        public bool? Deleted { get; set; }
+
+        [JsonProperty("profile")]
+        public Profile Profile { get; set; }
+
+        [JsonProperty("isBot")]
+        public bool? Is_bot { get; set; }
+
+        [JsonProperty("updated")]
+        public int? Updated { get; set; }
+
+        [JsonProperty("color")]
+        public string Color { get; set; }
+
+        [JsonProperty("real_name")]
+        public string RealName { get; set; }
+
+        [JsonProperty("tz")]
+        public string Tz { get; set; }
+
+        [JsonProperty("tz_label")]
+        public string TzLabel { get; set; }
+
+        [JsonProperty("tz_offset")]
+        public int? TzOffset { get; set; }
+
+        [JsonProperty("is_admin")]
+        public bool? IsAdmin { get; set; }
+
+        [JsonProperty("is_owner")]
+        public bool? IsOwner { get; set; }
+
+        [JsonProperty("is_primary_owner")]
+        public bool? IsPrimaryOwner { get; set; }
+
+        [JsonProperty("is_restricted")]
+        public bool? IsRestricted { get; set; }
+
+        [JsonProperty("is_ultra_restricted")]
+        public bool? IsUltraRestricted { get; set; }
+
+        [JsonProperty("has_2fa")]
+        public bool? Has2Fa { get; set; }
+
+        [JsonProperty("two_factor_type")]
+        public string TwoFactorType { get; set; }
+    }
+}
---
+++ SlackHistoryViewer.Slack/Models/Message.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using SlackHistoryViewer.Slack.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SlackHistoryViewer.Slack.Models
+{
+    public class Message
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("user")]
+        public string User { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
+
+        [JsonProperty("ts")]
+        public string TimeStamp { get; set; }
+
+        [JsonProperty("reactions")]
+        public IEnumerable<Reaction> Reactions { get; set; }
+
+        [JsonProperty("attachaments")]
+        public IEnumerable<Attachment> Attachments { get; set; }
+
+        [JsonProperty("edited")]
+        public Edited Edited { get; set; }
+    }
+}
