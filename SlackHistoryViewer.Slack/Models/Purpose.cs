da7bb2b4561b5958d3dee60cf9124926c15dc5d6
Sanziana <Sanziana@SANZI>
2017-05-17 00:18:26 +0300

    Added JsonProperty attribute

---
+++ SlackHistoryViewer.Slack/Models/Purpose.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SlackHistoryViewer.Slack.Models
+{
+    public class Purpose
+    {
+        [JsonProperty("value")]
+        public string Value { get; set; }
+
+        [JsonProperty("creator")]
+        public string Creator { get; set; }
+
+        [JsonProperty("last_set")]
+        public int? LastSet { get; set; }
+    }
+}
---
+++ SlackHistoryViewer.Slack/Models/Reaction.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SlackHistoryViewer.Slack.Models
+{
+    public class Reaction
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("users")]
+        public IEnumerable<string> Users { get; set; }
+
+        [JsonProperty("count")]
+        public int? Count { get; set; }
+    }
+}
