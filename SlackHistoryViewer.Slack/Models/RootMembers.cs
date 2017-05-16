d496e2f648c3df182d7b64dc54abd571d5d27d82
Sanziana <Sanziana@SANZI>
2017-05-17 00:21:34 +0300

    Added JsonProperty attribute

---
+++ SlackHistoryViewer.Slack/Models/RootMembers.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace SlackHistoryViewer.Slack.Models
+{
+    public class RootMembers
+    {
+        [JsonProperty("ok")]
+        public bool? Ok { get; set; }
+
+        [JsonProperty("members")]
+        public IEnumerable<Member> Members { get; set; }
+
+        [JsonProperty("cache_ts")]
+        public int CacheTimeStamp { get; set; }
+    }
+}
---
+++ SlackHistoryViewer.Slack/Models/RootMessages.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SlackHistoryViewer.Slack.Models
+{
+    public class RootMessages
+    {
+        [JsonProperty("ok")]
+        public bool? Ok { get; set; }
+
+        [JsonProperty("messages")]
+        public IEnumerable<Message> Messages { get; set; }
+
+        [JsonProperty("has_more")]
+        public bool? HasMore { get; set; }
+
+        [JsonProperty("is_limited")]
+        public bool? IsLimited { get; set; }
+    }
+}
