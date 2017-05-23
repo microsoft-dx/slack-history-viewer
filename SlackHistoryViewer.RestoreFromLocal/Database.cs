9226a951e4f06a3ad88a0cea61bf8739119f9e90
Sanziana <Sanziana@SANZI>
2017-05-23 17:30:51 +0300

    Optimised inserting into tables

--- SlackHistoryViewer.RestoreFromLocal/Database.cs
+++ SlackHistoryViewer.RestoreFromLocal/Database.cs
@@ -6,7 +6,7 @@ using Newtonsoft.Json;
 using SlackHistoryViewer.Database;
 using SlackHistoryViewer.Database.Models;
 using SlackHistoryViewer.Helpers;
-using SlackHistoryViewer.RestoreFromLocal.Configuration;
+using SlackHistoryViewer.Configuration;
 using SlackHistoryViewer.Slack.Models;
 
 namespace SlackHistoryViewer.RestoreFromLocal
@@ -17,25 +17,32 @@ namespace SlackHistoryViewer.RestoreFromLocal
         {
         }
 
-        public static void TruncateTable(string tableName)
+        public static void TruncateTables(params string[] tableNames)
         {
-            using (var dbContext = new SlackHistoryViewerDbContext(AppSettings.Instance.ConnectionString))
+            using (var dbContext = new SlackHistoryViewerDbContext())
             {
-                var sql = string.Format("DELETE FROM {0}", tableName);
-                dbContext.Database.ExecuteSqlCommand(sql);
+                var sql = string.Empty;
 
-                sql = string.Format("DBCC CHECKIDENT ({0}, RESEED, 0)", tableName);
-                dbContext.Database.ExecuteSqlCommand(sql);
+                foreach (var tableName in tableNames)
+                {
+                    sql = string.Format("DELETE FROM {0}", tableName);
+                    dbContext.Database.ExecuteSqlCommand(sql);
+
+                    sql = string.Format("DBCC CHECKIDENT ({0}, RESEED, 0)", tableName);
+                    dbContext.Database.ExecuteSqlCommand(sql);
+                }
             }
         }
 
         public static void InsertUsers(string path)
         {
-            using (var dbContext = new SlackHistoryViewerDbContext(AppSettings.Instance.ConnectionString))
+            using (var dbContext = new SlackHistoryViewerDbContext())
             {
                 var data = GetData<Member>(path, AppSettings.Instance.UsersFile);
 
                 dbContext.Users.AddRange(data.Select(u => new Users(u.Id, u.Name)));
+                dbContext.Users.Add(new Users(AppSettings.Instance.SlackBotId, AppSettings.Instance.SlackBotName));
+                dbContext.Users.Add(new Users(AppSettings.Instance.UnknownBotId, AppSettings.Instance.UnknownBotName));
 
                 dbContext.SaveChanges();
             }
@@ -43,7 +50,7 @@ namespace SlackHistoryViewer.RestoreFromLocal
 
         public static void InsertChannels(string path)
         {
-            using (var dbContext = new SlackHistoryViewerDbContext(AppSettings.Instance.ConnectionString))
+            using (var dbContext = new SlackHistoryViewerDbContext())
             {
                 var data = GetData<Channel>(path, AppSettings.Instance.ChannelsFile);
 
@@ -53,50 +60,45 @@ namespace SlackHistoryViewer.RestoreFromLocal
             }
         }
 
-        public static void AddMessage(Message message, string channelId)
+        public static void AddMessage(SlackHistoryViewerDbContext dbContext, Message message, string channelId)
         {
-            using (var dbContext = new SlackHistoryViewerDbContext(AppSettings.Instance.ConnectionString))
-            {
-                var newMessage = new Messages();
-                newMessage.MessageId = MD5Hasher.GetMd5Hash(message.User + message.TimeStamp);
+            var newMessage = new Messages();
+            newMessage.MessageId = MD5Hasher.GetMd5Hash(message.User + message.TimeStamp);
 
-                var idUser = dbContext.Users.Where(u => u.UserId == message.User)
+            var idUser = dbContext.Users.Where(u => u.UserId == message.User)
+                .Select(u => u.Id)
+                .FirstOrDefault();
+
+            if (idUser == 0)
+            {
+                var botId = dbContext.Users.Where(u => u.UserId == AppSettings.Instance.UnknownBotId)
                     .Select(u => u.Id)
                     .FirstOrDefault();
 
-                if (idUser == null)
-                {
-                    var botId = dbContext.Users.Where(u => u.UserId == AppSettings.Instance.SlackBotId)
-                        .Select(u => u.Id)
-                        .FirstOrDefault();
-
-                    idUser = botId;
-                }
-
-                newMessage.UserId = idUser;
-
-                var idChannel = dbContext.Channels.Where(c => c.ChannelId == channelId)
-                    .Select(c => c.Id)
-                    .FirstOrDefault();
-
-                newMessage.ChannelId = idChannel;
-                newMessage.JsonData = JsonConvert.SerializeObject(message);
-
-                dbContext.Messages.Add(newMessage);
-
-                dbContext.SaveChanges();
+                idUser = botId;
             }
+
+            newMessage.UserId = idUser;
+
+            var idChannel = dbContext.Channels.Where(c => c.ChannelId == channelId)
+                .Select(c => c.Id)
+                .FirstOrDefault();
+
+            newMessage.ChannelId = idChannel;
+            newMessage.JsonData = JsonConvert.SerializeObject(message);
+
+            dbContext.Messages.Add(newMessage);
         }
 
         public static void InsertMessages(string path)
         {
-            using (var dbContext = new SlackHistoryViewerDbContext(AppSettings.Instance.ConnectionString))
+            using (var dbContext = new SlackHistoryViewerDbContext())
             {
                 var files = Directory.GetFiles(@path, "*.*", SearchOption.AllDirectories);
 
                 foreach (string file in files)
                 {
-                    if (IsChannelsOrUsersFile(file))
+                    if (IsFileIgnored(file))
                     {
                         continue;
                     }
@@ -110,17 +112,31 @@ namespace SlackHistoryViewer.RestoreFromLocal
                     var text = File.ReadAllText(file);
                     var data = JsonConvert.DeserializeObject<List<Message>>(text);
 
-                    foreach (Message message in data)
+                    foreach (var message in data)
                     {
-                        AddMessage(message, channelId);
+                        if (IsAttachment(message))
+                        {
+                            continue;
+                        }
+
+                        AddMessage(dbContext, message, channelId);
                     }
+
+                    dbContext.SaveChanges();
                 }
             }
         }
 
-        private static bool IsChannelsOrUsersFile(string file)
+        private static bool IsAttachment(Message message)
         {
-            return file.EndsWith(AppSettings.Instance.UsersFile) || file.EndsWith(AppSettings.Instance.ChannelsFile);
+            return string.IsNullOrEmpty(message.Text) && message.Attachments.Any();
+        }
+
+        private static bool IsFileIgnored(string file)
+        {
+            return file.EndsWith(AppSettings.Instance.UsersFile) ||
+                file.EndsWith(AppSettings.Instance.ChannelsFile) ||
+                file.EndsWith(AppSettings.Instance.IntegrationLogsFile);
         }
 
         private static List<T> GetData<T>(string path, string filename)
