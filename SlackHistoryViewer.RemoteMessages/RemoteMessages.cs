f0cce85bc2f9546a9352411a7c6c9388e0006554
Sanziana <Sanziana@SANZI>
2017-08-10 09:04:16 +0200

    Extracted functionality to private methods

---
+++ SlackHistoryViewer.RemoteMessages/RemoteMessages.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using RemoteMessages;
+using SlackHistoryViewer.Database;
+using SlackHistoryViewer.Slack.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlackHistoryViewer.RemoteMessages
+{
+    internal class RemoteMessages
+    {
+
+        public async Task Run()
+        {
+            await addUsers();
+            await addChannels();
+            await addMessages();
+        }
+
+        private async Task addUsers()
+        {
+            var usersContent = await Requests.RequestUsersList();
+            var usersData = JsonConvert.DeserializeObject<RootMembers>(usersContent);
+
+            Database.InsertUsersWithoutDuplicates(usersData.Members);
+        }
+
+        private async Task addChannels()
+        {
+            var channelsContent = await Requests.RequestChannelsList();
+            var channelsData = JsonConvert.DeserializeObject<RootChannels>(channelsContent);
+
+            Database.InsertChannelsWithoutDuplicates(channelsData.Channels);
+
+        }
+
+        private async Task addMessages()
+        {
+            var channelsContent = await Requests.RequestChannelsList();
+            var channelsData = JsonConvert.DeserializeObject<RootChannels>(channelsContent);
+
+            foreach (Channel channel in channelsData.Channels)
+            {
+                var messagesContent = await Requests.RequestChannelsHistory(channel.Id);
+                var messagesData = JsonConvert.DeserializeObject<RootMessages>(messagesContent);
+                Database.InsertMessagesWithoutDuplicates(messagesData.Messages, channel.Id);
+            }
+
+        }
+
+    }
+}
