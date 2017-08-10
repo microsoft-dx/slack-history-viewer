2f1f716c2d96ac2a8080c295cd6009452271581a
Sanziana <Sanziana@SANZI>
2017-08-10 08:57:21 +0200

    Extracted RemoteMessages class to another file

--- SlackHistoryViewer.RemoteMessages/Program.cs
+++ SlackHistoryViewer.RemoteMessages/Program.cs
@@ -26,37 +26,7 @@ namespace SlackHistoryViewer.RemoteMessages
 
             Task.Run(() => new RemoteMessages().Run())
                 .Wait();
-            
+
         }
-
-        class RemoteMessages
-        {
-
-            public async Task Run()
-            {
-                using (var dbContext = new SlackHistoryViewerDbContext())
-                {
-
-                    //Adding users
-                    var usersContent = await Requests.RequestUsersList();
-                    var usersData = JsonConvert.DeserializeObject<RootMembers>(usersContent);
-                    Helpers.Database.InsertUsersWithoutDuplicates(usersData.Members);
-
-                    //Adding channels
-                    var channelsContent = await Requests.RequestChannelsList();
-                    var channelsData = JsonConvert.DeserializeObject<RootChannels>(channelsContent);
-                    Helpers.Database.InsertChannelsWithoutDuplicates(channelsData.Channels);
-
-                    //Adding messages
-                    foreach (Channel channel in channelsData.Channels)
-                    {
-                        var messagesContent = await Requests.RequestChannelsHistory(channel.Id);
-                        var messagesData = JsonConvert.DeserializeObject<RootMessages>(messagesContent);
-                        Helpers.Database.InsertMessagesWithoutDuplicates(messagesData.Messages, channel.Id);
-                    }
-                }
-            }
-        }
-
     }
 }
